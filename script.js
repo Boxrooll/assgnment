9de1e51c41149af01f56d304b85bdd1d57beea1d
Boxrooll <tony.corliss@stu.barren.kyschools.us>
2025-02-28 10:10:59 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// 1
+
+let jedi = [];
+console.log(jedi);
+
+jedi[Luke]
+
+const arrayNum1 = jedi.push(`Obi-Wan Kenobi`);
+
+const arrayNum2 = jedi.unshift(`Yoda`);
+
+console.log(jedi[2]);
+
+// 2
+
+let sithlords = ['Darth Vader','Darth Sidious','Darth Maul' ];
+console.log(sithlords);
+
+let imperialOfficers = ['Grand Moff Tarkin','Orson Krennic'];
+console.log(sithlords);
+
+const starWarsVillains = sithlords.concat(sithlords, imperialOfficers) .slice(0,24);
+console.log(starWarsVillains);
+[];
+
+// 3
+
+let droids = ['R2-D2','C-3PO','IG-88'];
+
+console.log(droids[1]);
+
+console.log(droids[2]);
+
+array.splice(10,5, IG-11)
